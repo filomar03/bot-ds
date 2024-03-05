f37e827517d52e0ae3d44c0c1c4b01c4df2ab111
filomar03 <unacreaturadivina@gmail.com>
2024-03-05 04:05:09 +0100

    basic functionalities finished
    def parameters should be tuned more

--- bot disconnetti delpu.js
+++ bot disconnetti delpu.js
@@ -1,51 +1,92 @@
-async function bot (name) {
-    const userClass = "voiceUser__0470a clickable__137bc userSmall_e40291"
-    const buttonId = "user-context-voice-disconnect"
-    const clickDelay = 200
+//Prolonged use of this script could make the account temporarily rate-limited
 
-    function lClick (element) {
-        let leftClickEvent = new MouseEvent('click', {
+let endSpam = false
+
+//clickDelay min value: 1, or it will cause errors when trying disconnecting a person that is already in the channel when starting bot, unknown cause
+//loopDelay min value: 0, no know issues
+//disconnectDelay min value: 300, reccomended value: 500, setting it below 300 will make the bot fail, since that is the average time for a user to be disconnected after clicking disconnect button
+const startSpamBot = (target, clickDelay = 1, loopDelay = 0, disconnectDelay = 0) => {
+    const usersClass = "voiceUser__0470a clickable__137bc userSmall_e40291"
+    const buttonId = "user-context-voice-disconnect"
+
+    function leftClickEvent() {
+        return new MouseEvent('click', {
             bubbles: true,
             cancelable: true,
             view: window,
             button: 0
         });
-
-        element.dispatchEvent(leftClickEvent);
     }
 
-    function rClick (element) {
-        let rightClickEvent = new MouseEvent('contextmenu', {
+    function rightClickEvent() {
+        return new MouseEvent('contextmenu', {
             bubbles: true,
             cancelable: true,
             view: window,
             button: 2
         });
-
-        element.dispatchEvent(rightClickEvent);
     }
-        
-    function getPeople () {
-        return document.getElementsByClassName(userClass)
+    
+    function getUsers() {
+        return document.getElementsByClassName(usersClass)
     }
 
-    function getDButton () {
+    function userName(user) {
+        return user.children[0].children[1].innerHTML
+    }
+
+    function getButton() {
         return document.getElementById(buttonId)
     }
 
-    function sleep (ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
+    let time = false
+
+    function loop() {
+        if (endSpam) {
+            console.log('Stopping spam.')
+            return
+        }
+        
+        let disconnecting = false
+
+        //TODO: remove (debug purpouse: find out avg time to disconnect user since disconnect button is clicked)
+        if (time) {
+            console.log(Date.now() - time)
+        }
+
+        Array.from(getUsers()).forEach(user => {
+            if (userName(user) == target) {
+                user.dispatchEvent(rightClickEvent())
+                new Promise(resolve => {
+                    setTimeout(() => {
+                        //TODO: remove (debug purpouse: find out avg time to disconnect user since disconnect button is clicked)
+                        if (!time) {
+                            time = Date.now()
+                        }
+
+                        getButton().dispatchEvent(leftClickEvent())
+                        resolve()
+                    }, clickDelay)
+                }).then(() => {
+                    setTimeout(loop, disconnectDelay)
+                })
+                disconnecting = true
+                return
+            }
+        })
+
+        if (!disconnecting) setTimeout(loop, loopDelay)
+
+        //TODO: remove (debug purpouse: find out avg time to disconnect user since disconnect button is clicked)
+        if (!disconnecting && time) {
+            console.log('time to disconnect: ' + Date.now() - time)
+            time = false
+        }
     }
 
-    //while (true) {
-        Array.from(getPeople()).forEach(element => {
-            if (element.children[0].children[1].innerHTML == name) {
-                rClick(element)
-                se
-                lClick(getDButton)
-            }
-        });
-        //lClick(getDButton())
-        //sleep?
-    //}
+    endSpam = false
+    console.log('Starting spam.')
+    loop()
 }
+
+const stopSpamBot = () => endSpam = true
